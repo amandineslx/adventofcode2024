3e318a9f6a496a85c7572d2eb89d43b6f09b0fbf
Amandine Souilleux <amandine.souilleux@dataiku.com>
2024-12-03 15:37:37 +0100

    day 3 part 2

--- 03/03.py
+++ 03/03.py
@@ -1,18 +1,27 @@
 import re
 
 INPUT_FILE = "03-input.txt"
-REGEX = r"mul\((\d{1,3}),(\d{1,3})\)"
+REGEX_MUL = r"mul\((\d{1,3}),(\d{1,3})\)"
+REGEX_IGNORE = r"don't\(\).*?(do\(\)|$)"
 
-def compute_sum():
-    line = ""
+def remove_ignored_instructions(instructions):
+    return re.sub(REGEX_IGNORE, "", instructions)
+
+def compute_sum(instructions):
+    sum = 0
+    for match in re.finditer(REGEX_MUL, instructions):
+        #print(match.group(0))
+        m = int(match.group(1))
+        n = int(match.group(2))
+        sum += n*m
+    return sum
+
+def main():
+    instructions = ""
     sum = 0
     with open(INPUT_FILE, 'r') as f:
         for line in f.readlines():
-            for match in re.finditer(REGEX, line):
-                print(match.group(0))
-                m = int(match.group(1))
-                n = int(match.group(2))
-                sum += n*m
+            sum += compute_sum(remove_ignored_instructions(line))
     print(sum)
 
-compute_sum()
+main()
