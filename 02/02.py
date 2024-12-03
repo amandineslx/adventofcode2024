5f6c33580787a60dc59675ebe211db34fe913598
Amandine Souilleux <amandine.souilleux@dataiku.com>
2024-12-03 14:11:44 +0100

    day 2 part 2

--- 02/02.py
+++ 02/02.py
@@ -5,31 +5,23 @@ INPUT_FILE = "02-input.txt"
 def is_safe(report):
     increase = None
     if len(set(report)) != len(report):
-        print(report)
-        print("unsafe for repetition")
         return False
     sorted_report = copy.deepcopy(report)
     sorted_report.sort()
     sorted_report_reverse = copy.deepcopy(report)
     sorted_report_reverse.sort(reverse=True)
     if report != sorted_report and report != sorted_report_reverse:
-        print(report)
-        print("unsafe for sign")
         return False
     for i in range(len(report)-1):
         diff = report[i] - report[i+1]
         if abs(diff) > 3:
-            print(report)
-            print("unsafe for gap")
             return False
-    #print("safe")
     return True
 
 def is_a_sub_report_safe(report):
-    for i in range(len(report)-1):
+    for i in range(len(report)):
         sub_report = copy.deepcopy(report)
         sub_report.pop(i)
-        #print(sub_report)
         if is_safe(sub_report):
             return True
     return False
@@ -39,8 +31,7 @@ def main():
     with open(INPUT_FILE, 'r') as f:
         for line in f.readlines():
             report = [int(x) for x in line[:-1].split(" ")]
-            #print(report)
-            if is_safe(report):
+            if is_safe(report) or is_a_sub_report_safe(report):
                 safe_reports += 1
     print(safe_reports)
 
